608d06296dce3587ed71598c008a43f6caa120b6
victorlistapad <victorlistapad@gmail.com>
2025-01-15 01:46:53 -0500

    New: - Created additional color scheme modes Updated: - Removed layout generation from render function and placed it in a createLayout() function

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
-const color = document.querySelector("#color-selector");
-const mode = document.querySelector("#color-scheme-mode-selector");
 document.querySelector("button").addEventListener("click", (e) => {
+  const color = document.querySelector("#color-selector");
+  const mode = document.querySelector("#color-scheme-mode-selector");
   e.preventDefault();
-  renderColorScheme(color.value.slice(1));
+  renderColorScheme(color.value.slice(1), mode.value);
 });
 
-// INITIAL RENDER
+// COLOR SCHEME RENDER
 function renderColorScheme(colorHexValue, schemeMode) {
   fetch(
-    `https://www.thecolorapi.com/scheme?hex=${colorHexValue}&mode=monochrome&count=4`
+    `https://www.thecolorapi.com/scheme?hex=${colorHexValue}&mode=${schemeMode}&count=4`
   )
     .then((res) => res.json())
     .then((colorArr) => {
-      let htmlComponents = ``;
       const { seed, colors } = colorArr;
-      console.log(seed, ...colors);
-      [seed, ...colors].forEach((color) => {
-        htmlComponents += `
+      document.querySelector(".colors-container").innerHTML = createLayout([
+        seed,
+        ...colors,
+      ]);
+    });
+}
+
+// COLOR SCHEME LAYOUT
+function createLayout(colorsArr) {
+  let htmlComponents = ``;
+  colorsArr.forEach((color) => {
+    htmlComponents += `
           <div class="single-color-container">
             <div class="color-option" style="background:${color.hex.value}"></div>
             <div class="hex-value-container">
@@ -24,9 +32,9 @@ function renderColorScheme(colorHexValue, schemeMode) {
             </div>
           </div>
         `;
-      });
-      document.querySelector(".colors-container").innerHTML = htmlComponents;
-    });
+  });
+  return htmlComponents;
 }
 
-renderColorScheme("F55A5A");
+// INITIAL RENDER
+renderColorScheme("F55A5A", "monochrome");
